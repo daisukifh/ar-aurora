40830a1a6cec2b87f9e6a975508d8ec06da48ce0
daisukifh <103920138+daisukifh@users.noreply.github.com>
2026-01-05 22:50:07 +0700

    Update SimpleBarcodeScanner.cs

--- Assets/Script/SimpleBarcodeScanner.cs
+++ Assets/Script/SimpleBarcodeScanner.cs
@@ -9,7 +9,6 @@ public class SimpleBarcodeScanner : MonoBehaviour
     {
         mBarcodeBehaviour = GetComponent<BarcodeBehaviour>();
     }
-
     // Update is called once per frame
     void Update()
     {
